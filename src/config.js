29489eaef9a664de55eaffb75c6983e9c3ea8f97
Tiago Almeida de Souza <tiagosouza@frameworksystem.com>
2020-07-16 17:57:53 -0300

    commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = "3e95fa2d-e6da-475f-adf6-bf691db1b83e";
 global.EMAIL_TEMPL = "<strong>Olá, {0}</strong> seja bem vindo a Node Store";
 
 module.exports = {
-    connectionString: "mongodb+srv://node:nodeSTR@cluster0-wpfys.azure.mongodb.net/test",
-    sendgridKey:"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",
-    containerConnectionString:"DefaultEndpointsProtocol=https;AccountName=nodefree;AccountKey=/rVqIzZFk8rhmSyCdZ9zJ5rdt1pAH/m2wcR1A6LSKsMrF+grpjNBr0N8IH2kDOGO+uR8FxbDdkzzaIzhePYw6g==;EndpointSuffix=core.windows.net"
+    connectionString: "xxxxx",
+    sendgridKey:"xxxxx",
+    containerConnectionString:"xxx"
 };
